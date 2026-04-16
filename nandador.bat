ea67a98e0ee751458ecafb11eccda1ca42d04f6b
guenterjuergen <guenther_juergen@tutamail.com>
2026-04-16 18:33:11 +0200

    made it a file spammer

--- nandador.bat
+++ nandador.bat
@@ -1,1 +1,8 @@
-echo "script executed, idiot!!" > %USERPROFILE%\Desktop\file.txt
+@echo off
+set iter=100
+:loop
+md %userprofile%\Desktop\%iter%
+set /a iter=iter-1 
+if %iter%==0 goto exit
+goto loop
+:exit
